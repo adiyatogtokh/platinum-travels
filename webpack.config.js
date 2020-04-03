cc1985f3936217b9e19c5119eff98b50355c49b9
Adiya <frozen.at34@gmail.com>
2020-04-04 00:01:58 +0800

    large hero section

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const postcssPlugins = [
+  require("postcss-import"),
+  require("postcss-mixins"),
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("autoprefixer")
+];
+module.exports = {
+  entry: "./app/assets/scripts/App.js", //ene folder-oos bundle hiine
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app")
+  },
+  devServer: {
+    before: function(app, server) {
+      server._watch("./app/**/*.html");
+    },
+    contentBase: path.join(__dirname, "app"),
+    hot: true,
+    port: 8000,
+    host: "0.0.0.0"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          { loader: "postcss-loader", options: { plugins: postcssPlugins } }
+        ]
+      }
+    ]
+  }
+};
