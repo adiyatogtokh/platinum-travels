21596bbf09b768478232fccfb1512676ea3e417a
Adiya <frozen.at34@gmail.com>
2020-04-04 13:02:20 +0800

    dood taliin 4n paragrah hiij duusgv

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,22 @@ const postcssPlugins = [
   require("postcss-mixins"),
   require("postcss-simple-vars"),
   require("postcss-nested"),
-  require("autoprefixer")
+  require("autoprefixer"),
 ];
 module.exports = {
   entry: "./app/assets/scripts/App.js", //ene folder-oos bundle hiine
   output: {
     filename: "bundled.js",
-    path: path.resolve(__dirname, "app")
+    path: path.resolve(__dirname, "app"),
   },
   devServer: {
-    before: function(app, server) {
+    before: function (app, server) {
       server._watch("./app/**/*.html");
     },
     contentBase: path.join(__dirname, "app"),
     hot: true,
     port: 8000,
-    host: "0.0.0.0"
+    //host: "0.0.0.0"
   },
   mode: "development",
   module: {
@@ -29,9 +29,9 @@ module.exports = {
         use: [
           "style-loader",
           "css-loader",
-          { loader: "postcss-loader", options: { plugins: postcssPlugins } }
-        ]
-      }
-    ]
-  }
+          { loader: "postcss-loader", options: { plugins: postcssPlugins } },
+        ],
+      },
+    ],
+  },
 };
